684258031dc5225f9a2106cfe3b9f22a00b0f3f0
Calvinnnn <oemad3366@gmail.com>
2026-04-09 16:39:13 +0200

    http response define status code by extracting response url destination from http request

--- src/response.c
+++ src/response.c
@@ -5,8 +5,27 @@
 
 
 void SendHTTPResponse(int client_fd){
-    const char *response =
-    "HTTP/1.1 200 OK\r\n\r\n";
-    send(client_fd,response,strlen(response),0);
 
+    char *response =
+    "HTTP/1.1 200 OK\r\n\r\n";
+    char buffer[4096];
+    char method[10],version[20],path[100];
+   
+    
+    int bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+
+    if(bytes_received >0){
+        buffer[bytes_received] ='\0';
+        sscanf(buffer,"%s %s %s",method,path,version);
+        if(strcmp(path,"/")==0){
+            send(client_fd,response,strlen(response),0);
+        }
+        else{
+            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            send(client_fd,response,strlen(response),0);
+        }
+    }
+
+
+    
 }
